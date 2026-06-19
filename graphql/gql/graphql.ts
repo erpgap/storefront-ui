303e51e4483a11383925d5bb69ce6a08d4297af3
Lucas Soares <soareslssa.dev@gmail.com>
2026-06-19 15:23:50 -0300

    refactor(graphql): rename author field in BlogPost type to authorId

--- graphql/gql/graphql.ts
+++ graphql/gql/graphql.ts
@@ -97,7 +97,7 @@ export type AttributeValue = {
 
 export type BlogPost = {
   __typename?: 'BlogPost';
-  author: Maybe<Partner>;
+  authorId: Maybe<Partner>;
   content: Maybe<Scalars['String']['output']>;
   id: Maybe<Scalars['Int']['output']>;
   image: Maybe<Scalars['String']['output']>;
